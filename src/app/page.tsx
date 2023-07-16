606524d1459465c935a7d7acf864731daa219286
nauval <nauvalyusufad@gmail.com>
2023-07-16 22:18:32 +0700

    so far so good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+export default function Homepage() {
+  const [darkmode, setDarkmode] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className={`${darkmode ? "dark" : ""}`}>
+        <div className="w-screen flex justify-center bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+          <div className="w-[1024px] min-h-screen dark:bg-slate-800 bg-white dark:text-white text-slate-800">
+            <Navbar
+              darkmode={darkmode}
+              setDarkmode={() => setDarkmode(!darkmode)}
             />
-          </a>
+          </div>
         </div>
       </div>
+    </>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+function Navbar({
+  darkmode,
+  setDarkmode,
+}: {
+  darkmode: boolean;
+  setDarkmode: <T extends boolean>(V: T) => void;
+}) {
+  const genericHamburgerLine = `w-[65%] h-[0.25rem] dark:bg-white bg-slate-800  my-[2px]  transition ease transform duration-300`;
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <div className="hidden md:block ">
+        {/* Navbar */}
+        <div className="flex flex-col max-w-[1024px] ">
+          <div className="px-[16px]">
+            <div className=" dark:bg-slate-800 bg-white w-full m-0 h-[72px] text-[22px] flex flex-row items-center py-[5px]">
+              {/* Navbar logo  */}
+              <div className=" w-[50%]  flex flex-row space-x-3 items-center  ">
+                <div className="rounded-[50%] h-[54px] w-[54px] flex items-center justify-center ~~~ dark:bg-white bg-slate-800 dark:text-slate-800 text-white  ~~~~">
+                  <h1>md</h1>
+                </div>
+                <div> Nauval Yusuf Addairy</div>
+              </div>{" "}
+              {/* Darkmode Toogle */}
+              <div className="w-[60%] h-[100px] flex flex-row items-center  justify-end space-x-[20px] mr-[10px]">
+                <div onClick={() => setDarkmode(!darkmode)} className="">
+                  {" "}
+                  <div
+                    className={`w-[28px] h-[28px] bg-slate-800/40 dark:bg-white/40 rounded-2xl flex items-center flex-row px-[2px] transition duration-500 ease-in-out`}
+                  >
+                    <div className={`h-[24px] w-[24px] rounded-[50%]`}>
+                      {darkmode ? (
+                        <MoonIcon className="h-[24px] w-[24px] text-yellow-400" />
+                      ) : (
+                        <SunIcon className="h-[24px] w-[24px] text-yellow-400" />
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {/* menu bar */}
+                <div className="">about</div>
+                <div className="">project</div>
+                <div className="">blogs</div>
+                <div className=" py-[2px] px-[8px] rounded-md text-white  animate-pulse bg-gradient-to-tr from-green-800 via-blue-600 to-purple-700 dark:from-green-600 dark:via-blue-500 dark:to-purple-600 ">
+                  Contact
+                </div>
+              </div>
+            </div>
+            {/* About section for md */}
+
+            <div className="hidden md:block ">
+              <div className="w-full h-[500px] mt-[42px] bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-indigo-300 to-purple-400 rounded-xl ">
+                <div className="w-full h-full dark:bg-slate-800/25 bg-white/25 backdrop-blur-sm px-[8px] py-[6px] rounded-xl"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="xl:hidden md:hidden lg:hidden">
+        <div className="w-full h-[42px] flex flex-row px-[12px] py-[8px] items-center">
+          <div className="w-[65%] flex flex-row space-x-[8px] items-center">
+            <div className=" w-[36px] h-[36px] rounded-[50%] dark:bg-white bg-slate-800 dark:text-slate-800 text-white  flex items-center justify-center">
+              ADW
+            </div>
+            <span>Nauval Yusuf Affa</span>
+          </div>
+          <div onClick={() => setDarkmode(!darkmode)} className="w-[25%]">
+            <div
+              className={`w-[55%] h-[24px] rounded-2xl bg-slate-800/50 flex flex-row p-[2px] dark:bg-white/50 items-center ${
+                darkmode ? "justify-end" : "justify-start"
+              }`}
+            >
+              {darkmode ? (
+                <MoonIcon className="w-[24px] h-[24px] text-yellow-400" />
+              ) : (
+                <SunIcon className="w-[24px] h-[24px] text-yellow-300" />
+              )}
+            </div>
+          </div>
+          <div
+            onClick={() => setOpen(!open)}
+            className="w-[10%] h-[36px] flex flex-col justify-center items-center  group"
+          >
+            <div
+              className={`${genericHamburgerLine} ${
+                open
+                  ? "rotate-45 translate-y-[0.5rem] opacity-50 group-hover:opacity-100"
+                  : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`${genericHamburgerLine} ${
+                open ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`${genericHamburgerLine} ${
+                open
+                  ? "-rotate-45 -translate-y-[0.5rem] opacity-50 group-hover:opacity-100"
+                  : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+          </div>
+        </div>
+        {open ? (
+          <>
+            <div className="w-full h-[100px] dark:bg-white/25 bg-slate-800/10 flex flex-col items-center  justify-around transition animate-tray">
+              <div className=" animate-tray">about</div>
+              <div className=" animate-tray">project</div>
+              <div className=" animate-tray">blogs</div>
+              <div className=" animate-tray">contact</div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
-    </main>
-  )
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens:{
+      'xs':"375px",
+      'sm':"425px",
+      "md":"768px",
+      "lg":"1024px",
+      "xl":"1440px",
+      "2xl":"2560px"
+
+     },
     extend: {
+     
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation:{
+        tray: 'tray 0.5s ease-in-out'
+      },
+      keyframes:{
+        tray:{
+          '0%': {
+            'height':'0px'
+          },
+          '100%':{
+            'height':"100px"
+          }
+        }
+      }
     },
   },
   plugins: [],
