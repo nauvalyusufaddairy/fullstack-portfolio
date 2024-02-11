715a4b12ceffa8b928626796b8fa3ff5564a6ee9
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-02-11 20:22:53 +0700

    done

--- src/canvas/stars-canvas.tsx
+++ src/canvas/stars-canvas.tsx
@@ -3,8 +3,7 @@ import { useState, useRef, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
 import Stars from '@/gltftsx/stars'
-import { Globe } from '@/gltftsx/globe'
-import GlobCanvas from './globe-canvas'
+
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
