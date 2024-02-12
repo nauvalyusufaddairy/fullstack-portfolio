f2df5c9e19802a6d45003769309767918d5d8c25
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2024-02-12 22:53:13 +0700

    dev net

--- src/actions/send-email.ts
+++ src/actions/send-email.ts
@@ -4,40 +4,39 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses'
 import { EmailSchema } from '@/lib/schemas'
 
 export default async function sendEmail(values: zod.infer<typeof EmailSchema>) {
-  try {
-    const data = await EmailSchema.parseAsync(values)
-    const { email, message, subject } = data
+  const data = await EmailSchema.parseAsync(values)
+  const { email, message, subject } = data
 
-    const sesClient = new SESClient({
-      region: 'us-east-1',
-      credentials: {
-        accessKeyId: process.env.AACCESS_KEY_ID!,
-        secretAccessKey: process.env.AACCESS_KEY_SECRET!
-      }
-    })
+  const sesClient = new SESClient({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.AACCESS_KEY_ID!,
+      secretAccessKey: process.env.AACCESS_KEY_SECRET!
+    }
+  })
 
-    const params = {
-      Destination: {
-        ToAddresses: ['nauvalyusufad@gmail.com']
-      },
-      Message: {
-        Body: {
-          Text: {
-            Data: message + `\n \n sended from` + ' ' + email
-          }
-        },
-        Subject: {
-          Data: subject
+  const params = {
+    Destination: {
+      ToAddresses: ['nauvalyusufad@gmail.com']
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: message + `\n \n sended from` + ' ' + email
         }
       },
-      Source: 'no-reply@nauvalyusufaddairy.com' // Replace with your sender email address
-    }
-
+      Subject: {
+        Data: subject
+      }
+    },
+    Source: 'no-reply@nauvalyusufaddairy.com' // Replace with your sender email address
+  }
+  try {
     const command = new SendEmailCommand(params)
     const result = await sesClient.send(command)
-
-    return { succes: `Email sent` }
+    const res = result
+    return { succes: JSON.stringify(res) }
   } catch (error) {
-    return { e: error }
+    return { e: JSON.stringify(error) }
   }
 }
